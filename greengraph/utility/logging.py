320b66a2916df8723ee7c68ca50890ae3c406d59
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2025-05-22 15:44:18 +0200

    updated logtimer

--- greengraph/utility/logging.py
+++ greengraph/utility/logging.py
@@ -7,8 +7,17 @@ from datetime import datetime
 def logtimer(message):
     start_time = datetime.now()
     logging.info(f"{start_time.strftime('%H:%M:%S')}: Started {message}")
-    yield
-    end_time = datetime.now()
-    duration = (end_time - start_time).total_seconds()
-    minutes, seconds = divmod(duration, 60)
-    logging.info(f"{end_time.strftime('%H:%M:%S')}: Completed {message} ({int(minutes):02d}:{int(seconds):02d} min:sec}")
+    
+    operation_succeeded = False
+    try:
+        yield
+        operation_succeeded = True
+    finally:
+        end_time = datetime.now()
+        duration = (end_time - start_time).total_seconds()
+        minutes, seconds = divmod(duration, 60)
+        
+        if operation_succeeded:
+            logging.info(f"{end_time.strftime('%H:%M:%S')}: Completed {message} ({int(minutes):02d}:{int(seconds):02d} min:sec)")
+        else:
+            logging.error(f"{end_time.strftime('%H:%M:%S')}: Failed {message} after ({int(minutes):02d}:{int(seconds):02d} min:sec)")
